fb933d9cb184f4e3d15eb40a94bcc9b76d4d93de
Felipe Rocha de Lucena - 40259869813 <felipe.lucena@anima.intranet>
2021-03-18 08:43:09 -0300

    Adicionado menu de categorias

--- header.php
+++ header.php
@@ -54,3 +54,12 @@
       </nav>
     </header>
 
+    <?php 
+      wp_nav_menu([
+        'menu' => 'categorias',
+        'container' => 'nav',
+        'container_class' => 'menu-categorias',
+      ]);
+    ?>
+
+
