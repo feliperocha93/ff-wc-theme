6e6b54c7a32428d0cfb2ee833b08d5d050085760
Felipe Rocha de Lucena - 40259869813 <felipe.lucena@anima.intranet>
2021-03-21 12:27:31 -0300

    Adicionado slider na home

--- page-home.php
+++ page-home.php
@@ -1,8 +1,52 @@
 <?php
 // Template Name: Home
-get_header();
+get_header(); ?>
 
-echo "Página Home";
+<?php 
+$produtos_slide = wc_get_products([
+  'limit' => 6,
+  'tag' => ['slide'],
+]);
 
-get_footer();
+function formatar_produtos($produtos) {
+  $produtos_formatados = [];
+  foreach($produtos as $produto) {
+    $produtos_formatados[] = [
+      'nome' => $produto->get_name(),
+      'preco' => $produto->get_price_html(),
+      'link' => $produto->get_permalink(),
+      'img' => wp_get_attachment_image_src($produto->get_image_id(), 'slide')[0],
+    ];
+  }
+  return $produtos_formatados;
+}
+
+$produtos_formatados = formatar_produtos($produtos_slide);
 ?>
+
+<?php if(have_posts()) { while (have_posts()) {the_post(); ?>
+
+<ul class="vantagens">
+  <li>Frete Grátis</li>
+  <li>Troca fácil</li>
+  <li>Até 12x</li>
+</ul>
+
+<section class="slide-wrapper">
+  <ul class="slide">
+    <?php foreach($produtos_formatados as $produto) { ?>
+      <li class="slide-item">
+        <img src="<?= $produto['img']; ?>" alt="<?= $produto['nome']; ?>">
+        <div class="slide-info">
+          <span class="slide-preco"><?= $produto['preco']; ?></span>
+          <h2 class="slide-nome"><?= $produto['nome']; ?></h2>
+          <a href="<?= $produto['link']; ?>" class="slide-link">Ver Produto</a>
+        </div>
+      </li>
+    <?php } ?>
+  </ul>
+</section>
+
+<?php } } ?>
+
+<?php get_footer(); ?>
